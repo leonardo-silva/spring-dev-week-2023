892b4a1db0095fe5403a8d2f98d5711694b2a52f
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-11-26 14:21:00 -0300

    feat: implementing domain layer

---
+++ src/main/java/com/example/devweek2023/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package com.example.devweek2023.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/example/devweek2023/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.devweek2023.domain.repository;
+
+import com.example.devweek2023.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String accountNumber);
+}
